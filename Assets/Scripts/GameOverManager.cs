fc19420391ff66956f9205c457e6189585155f01
Anders <lordpeppezaltaz22@hotmail.com>
2017-03-21 19:50:56 +0100

    sideboundaries fixed

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverManager : MonoBehaviour {
 
@@ -12,9 +13,14 @@ private float restartDelay = 5.0f; //timer used for time before restartDelay
 
 [SerializeField]
 private float restartTimer; //timer to count up to restarting the level 
+
+[SerializeField]
+private Text lblGameOver; 
+
 	// Use this for initialization
 	void Start () {
 	 	playerScript = FindObjectOfType<PlayerBehaviour>();
+		lblGameOver.enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -25,12 +31,14 @@ private float restartTimer; //timer to count up to restarting the level
 
 		if (playerHealth <= 0) {
 			
-			//increment timer to count up to restart
-			restartTimer += Time.deltaTime;
+			
+			restartTimer += Time.deltaTime; //increment timer to count up to restart
+
+			lblGameOver.enabled = true;
 
 			if (restartTimer > restartDelay) {
-				//loads the current scene
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+			
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);	//loads the current scene
 			}
 		}
 	}
